1afa01a2d724c3d08caadc0a23a93c0a841dde9e
Salohiddin0l0 <msalohiddin310@gmail.com>
2025-09-06 20:37:01 +0500

    Created new url
    url

--- myapp4/urls.py
+++ myapp4/urls.py
@@ -5,4 +5,7 @@ from django.urls import path
 urlpatterns = [
     path('', index, name='index'),
     path('register', register, name='register'),
+    path('login', login, name='login'),
+    path('filter-author', filter_author, name='filter-author'),
+
 ]
--- myapp4/views.py
+++ myapp4/views.py
@@ -17,5 +17,24 @@ def register(request):
 
     return render(request, 'register.html', ctx)
 
+def login(request):
+    ctx = {
 
+    }
+
+    return render(request, 'login.html', ctx)
+
+def filter_author(request):
+    ctx = {
+
+    }
+
+    return render(request, 'filter_author.html', ctx)
+
+def register(request):
+    ctx = {
+
+    }
+
+    return render(request, 'register.html', ctx)
 
