68d6896e37519ae037724cb7205d0e76dcbf1c6f
Salohiddin0l0 <msalohiddin310@gmail.com>
2025-09-08 22:45:45 +0500

    major changes
    new models

--- myapp4/models.py
+++ myapp4/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Add_Post(models.Model):
+    sarlavha = models.CharField(max_length=128, null=False)
+    matn = models.TextField
+    image = models.ImageField(upload_to="post")
+
+class Comment(models.Model):
+    user = models.CharField(max_length=128)
+    message = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    new = models.ForeignKey(Add_Post, on_delete=models.CASCADE)
+
+
