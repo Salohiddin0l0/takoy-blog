898b926a87fdf5664846847b24995efd9e031f38
Salohiddin0l0 <msalohiddin310@gmail.com>
2025-09-08 22:48:34 +0500

    some changes in model
    adding some features

--- myapp4/models.py
+++ myapp4/models.py
@@ -5,6 +5,8 @@ class Add_Post(models.Model):
     sarlavha = models.CharField(max_length=128, null=False)
     matn = models.TextField
     image = models.ImageField(upload_to="post")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
     user = models.CharField(max_length=128)
